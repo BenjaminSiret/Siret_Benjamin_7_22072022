789a9087041840e653c21032bf925cdcbe541adf
Benjamin Siret <benjaminsiret@outlook.fr>
2022-08-12 16:56:08 +0200

    sort js files, add favicon

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,5 @@
 const query = "";
 
-function searchListener(recipes) {
-  const searchInput = document.getElementById("search-input");
-  searchInput.addEventListener("keyup", () => {
-    let query = searchInput.value;
-    if (query.length > 2) {
-      const searchResults = searchRecipes(recipes, query);
-      if (!searchResults.length) {
-        console.log(
-          "Aucune recette ne correspond à votre recette, vous pouvez chercher 'tarte aux pommes', 'poisson', ..."
-        );
-        displayRecipes(recipes);
-      } else {
-        document.querySelector(".results").innerHTML = "";
-        displayRecipes(searchResults);
-      }
-    }
-  });
-}
-
 async function getRecipes() {
   const url = "data/recipes.json";
   const response = await fetch(url);
