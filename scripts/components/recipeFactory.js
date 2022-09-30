da233c0755f7c38c03469b127c7ee0853283bcb1
Benjamin Siret <benjaminsiret@outlook.fr>
2022-09-30 14:20:09 +0200

    design recipe card

--- scripts/components/recipeFactory.js
+++ scripts/components/recipeFactory.js
@@ -13,13 +13,32 @@ function recipeFactory(data) {
   function getRecipeCardDOM() {
     const card = document.createElement("article");
     card.classList.add("card");
-    card.innerHTML = `<div class="card-content">
+    card.innerHTML = `<div class="card-img"></div>
+                      <div class="card-content">
                         <div class="card-content-header">
                           <h2 class="recipe-title">${name}</h2>
-                          <p class="recipe-time">${time}</p>
+                          <div class="recipe-duration">
+                            <img src='assets/clock.png' alt="clock">
+                            <p class="recipe-time">${time} min</p>
+                          </div>
                         </div>
                         <div class="card-content-text">
-                          <p class="recipe-text">${description}</p>
+                          <ul class="recipe-ingredients">
+                            ${ingredients.map((ingredient) => {
+                                if (ingredient.unit === "grammes") {
+                                  return `<li><strong>${ingredient.ingredient}:</strong>
+                                                      ${ingredient.quantity ? ingredient.quantity : ""}gr
+                                          </li>`;
+                                } else {
+                                  return `<li><strong>${ingredient.ingredient}:</strong>
+                                                      ${ingredient.quantity ? ingredient.quantity : ""}
+                                                      ${ingredient.unit ? ingredient.unit : ""}
+                                          </li>`;
+                                }
+                            }).join("")
+                            }
+                          </ul>
+                          <div class="recipe-text">${description}</div>
                         </div>
                       </div>`;
     return card;
@@ -36,5 +55,3 @@ function recipeFactory(data) {
     getRecipeCardDOM,
   };
 }
-
-
